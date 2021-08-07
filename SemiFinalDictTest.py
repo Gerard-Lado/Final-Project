a2bb8feef3ec00b6081013a22058a6117cfc093e
Gerard-Lado <86641276+Gerard-Lado@users.noreply.github.com>
2021-08-07 17:10:48 +0800

    Add files via upload

--- SemiFinalDictTest.py
+++ SemiFinalDictTest.py
@@ -54,7 +54,100 @@ class DictTest(App):
         if store.exists((self.user.text).lower()):
             self.greeting.text = "The word" + " " + self.user.text + " " + "exists."
         else:
-            self.greeting.text = "The word" + " " + self.user.text + " " + "does not exist."
+            #Suggested Words
+            searched_word = (self.user.text).lower()
+            tested_word_letters = []
+            searched_word_letters = []
+            tested_word = ""
+
+            for letter in searched_word: #This allows the letters to revert to the original input after going through all the alternatives
+                searched_word_letters.append(letter)
+
+            for letter in searched_word: #This is what's actually being tested down there \/
+                tested_word_letters.append(letter)
+
+            print(tested_word_letters)
+            print(searched_word_letters)
+            suggested_words = []
+
+            keyboard_approximate = {
+                "a": "qwesxz",
+                "b": "vghn",
+                "c": "xdfv",
+                "d": "wersfxcv",
+                "e": "wsdfr",
+                "f": "ertdgcvb",
+                "g": "rtyfhvbn",
+                "h": "tyugjbnm",
+                "i": "ujklo",
+                "j": "yuihknm",
+                "k": "uiojlm",
+                "l": "kiop",
+                "m": "nhjkl",
+                "n": "bhjm",
+                "o": "iklp",
+                "p": "ol",
+                "q": "asw",
+                "r": "edfgt",
+                "s": "qazxcdew",
+                "t": "rfghy",
+                "u": "yhjki",
+                "v": "cfgb",
+                "w": "qasde",
+                "x": "zasdc",
+                "y": "tghju",
+                "z": "asx"
+            }
+
+            vowel_approximate = {
+                "a": "eiou",
+                "e": "aiou",
+                "i": "aeou",
+                "o": "aeiu",
+                "u": "aeio"
+            }
+
+            for a in range(len(searched_word_letters)):
+
+            #------------ Checking for slight mispells due to misclicks -------------    
+                for k, v in keyboard_approximate.items():
+                    if tested_word_letters[a] == k:
+                        for approx_letter in v:
+                            tested_word_letters = searched_word_letters.copy()
+                            tested_word_letters[a] = approx_letter
+                            tested_word = ""
+                            for i in tested_word_letters:
+                                tested_word = tested_word + i
+                            if store.exists(tested_word):
+                                suggested_words.append(tested_word)
+                            else:
+                                pass
+            #------------------------------------------------------------------------
+                tested_word_letters = searched_word_letters.copy() #Reset tested list to original
+
+            #----------- Checking for mispells due to mixing vowels around ----------             
+                for y, z in vowel_approximate.items():
+                    if tested_word_letters[a] == y:
+                        print("detected")
+                        for approx_vowel in z:
+                            tested_word_letters = searched_word_letters.copy()
+                            tested_word_letters[a] = approx_vowel
+                            tested_word = ""
+                            for h in tested_word_letters:
+                                tested_word = tested_word + h
+                            print(tested_word)
+                            if store.exists(tested_word):
+                                suggested_words.append(tested_word)
+                            else:
+                                pass
+            #------------------------------------------------------------------------
+                tested_word_letters = searched_word_letters.copy() #Reset tested list to original
+
+            suggested_words = list(set(suggested_words))
+            self.greeting.text = "The word " + self.user.text + " does not exist."
+            print(f"Perhaps you meant to type {suggested_words}")
+            
+    
 
 # run Say Hello App Calss
 if __name__ == "__main__":
